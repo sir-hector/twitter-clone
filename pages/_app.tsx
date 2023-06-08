7fe0fe6d357b376874ef2899871582a4d48bbf0a
sir-hector <krauskarol7@gmail.com>
2023-06-08 19:18:26 +0200

    Add modal component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    
+    <Modal isOpen={true} title='test modal' actionLabel='submit'/>
     <Layout>
       <Component {...pageProps} />
     </Layout>
