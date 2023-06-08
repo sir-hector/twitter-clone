2205db43f441aa06d92054bd21a6b6abad01a4a7
sir-hector <krauskarol7@gmail.com>
2023-06-08 14:16:37 +0200

    create button component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,12 @@ import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 
 }
