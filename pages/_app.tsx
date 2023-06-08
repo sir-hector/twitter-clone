a6656ca5409e4e976a8b74658039253104194f1c
sir-hector <krauskarol7@gmail.com>
2023-06-08 23:29:02 +0200

    add login model with hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import Modal from '@/components/Modal'
+import LoginModal from '@/modals/LoginModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <LoginModal/>
     <Layout>
       <Component {...pageProps} />
     </Layout>
