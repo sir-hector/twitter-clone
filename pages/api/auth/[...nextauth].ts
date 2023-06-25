258d4118eda2a894de47a7c3e95a792b23434804
sir-hector <krauskarol7@gmail.com>
2023-06-25 21:00:54 +0200

    create register handler

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -49,3 +49,4 @@ export default NextAuth({
   },
   secret: process.env.NEXTAUTH_SECRET
 });
+
---
+++ pages/api/register.ts
@@ -0,0 +1,28 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import bcrypt from 'bcrypt';
+import prisma from '@/libs/prismadb'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    if(req.method != 'POST') {
+        return res.status(405).end();
+    }
+
+    try {
+        const {email, username, name, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await prisma?.user.create({
+            data: {
+                email,
+                username,
+                name,
+                hashedPassword
+            }
+        })
+    } catch (error){
+        console.log(error)
+    }
+}
