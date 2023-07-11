0d414d7de3587e66f7f8b18f748532b357e1b397
sir-hector <krauskarol7@gmail.com>
2023-07-11 23:04:33 +0200

    add toast to register modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,23 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
-import Layout from '@/components/Layout'
-import Modal from '@/components/Modal'
-import LoginModal from '@/modals/LoginModal'
-import RegisterModal from '@/modals/RegisterModal'
+import "@/styles/global.css";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
+import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
+import LoginModal from "@/modals/LoginModal";
+import RegisterModal from "@/modals/RegisterModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <LoginModal/>
-    <RegisterModal/>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <SessionProvider session={pageProps.session}>
+        <Toaster />
+        <LoginModal />
+        <RegisterModal />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
     </>
-  )
-
+  );
 }
