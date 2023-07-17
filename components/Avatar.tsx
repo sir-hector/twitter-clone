46e3b5f5285d4519523323cdd23edc557e197f01
sir-hector <krauskarol7@gmail.com>
2023-07-17 21:45:59 +0200

    create avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -26,7 +26,7 @@ const Avatar: React.FC<AvatarProps> = ({userId, isLarge, hasBorder}) => {
         ${hasBorder ? 'border-4 border-black' : ''}
         ${isLarge ? 'h-32' : 'h-12'}
         ${isLarge ? 'w-32' : 'w-12'}
-        rouded-full
+        rounded-full
         hover-opacity-90
         cursor-pointer
         transition
