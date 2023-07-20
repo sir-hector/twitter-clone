01c9992a02fef75c6407fb0590735eb88b37c845
sir-hector <krauskarol7@gmail.com>
2023-07-20 23:14:54 +0200

    create form for post creating

---
+++ components/Form.tsx
@@ -0,0 +1,53 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import usePosts from "@/hooks/useHooks";
+import useLoginModel from "@/hooks/useLoginModel";
+import useRegisterModal from "@/hooks/useRegisterModal";
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import Button from "./Button";
+
+interface FormProps {
+  placeholder: string;
+  isComment?: boolean;
+  postId?: string;
+}
+
+const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModel();
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutatePosts } = usePosts();
+
+  const [body, setBody] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      await axios.post("api/post", { body });
+      toast.success("Tweet created");
+      setBody('');
+      mutatePosts('');
+
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [body, mutatePosts]);
+
+  return (
+    <div className="border-b-[1px] border-neutral-800 px-5 py-2">
+        <div className="py-8">
+            <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to Twitter</h1>
+        <div className="flex flex-row items-center justify-center gap-4">
+            <Button label="Login" onClick={loginModal.onOpen}/>
+            <Button label="Register" onClick={registerModal.onOpen} secondary/>
+        </div>
+        </div>
+    </div>
+  );
+};
+
+export default Form;
--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -49,7 +49,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 })
             }
 
-            return res.status(200).json(posts)
+            return res.status(200).json(posts);
         }
 
     }catch(error){
