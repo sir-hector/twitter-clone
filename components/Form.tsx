d302f06f0d586e371d39bd54665a830ecc8476fb
sir-hector <krauskarol7@gmail.com>
2023-07-24 18:32:53 +0200

    create form to create posts

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,11 +1,12 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
-import usePosts from "@/hooks/useHooks";
+import usePosts from "@/hooks/usePosts";
 import useLoginModel from "@/hooks/useLoginModel";
 import useRegisterModal from "@/hooks/useRegisterModal";
 import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
 import axios from "axios";
 import Button from "./Button";
+import Avatar from "./Avatar";
 
 interface FormProps {
   placeholder: string;
@@ -25,11 +26,10 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
-      await axios.post("api/post", { body });
+      await axios.post("api/posts", { body });
       toast.success("Tweet created");
-      setBody('');
-      mutatePosts('');
-
+      setBody("");
+      mutatePosts("");
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
@@ -39,13 +39,42 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
+      {currentUser ? (
+        <div className="flex flex-row gap-4">
+          <div>
+            <Avatar userId={currentUser?.id} />
+          </div>
+          <div className="w-full">
+            <textarea
+              disabled={isLoading}
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+              className="disabled:opacity-80 peer resize-none mt-3 w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500
+              text-white"
+              placeholder={placeholder}
+            ></textarea>
+            <hr className="oapcity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
+            <div className="mt-4 flex flex-row justify-end">
+              <Button
+                disabled={isLoading || !body}
+                onClick={onSubmit}
+                label="Tweet"
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
         <div className="py-8">
-            <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to Twitter</h1>
-        <div className="flex flex-row items-center justify-center gap-4">
-            <Button label="Login" onClick={loginModal.onOpen}/>
-            <Button label="Register" onClick={registerModal.onOpen} secondary/>
+          <h1 className="text-white text-2xl text-center mb-4 font-bold">
+            Welcome to Twitter
+          </h1>
+          <div className="flex flex-row items-center justify-center gap-4">
+            <Button label="Login" onClick={loginModal.onOpen} />
+            <Button label="Register" onClick={registerModal.onOpen} secondary />
+          </div>
         </div>
-        </div>
+      )}
+      ś
     </div>
   );
 };
