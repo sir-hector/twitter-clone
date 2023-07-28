b052f71cd13caa27f29fff456d6ede9b1a641650
sir-hector <krauskarol7@gmail.com>
2023-07-28 17:23:37 +0200

    notification feed component

--- components/NotificationFeed.tsx
+++ components/NotificationFeed.tsx
@@ -1,8 +1,37 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useNotifications from "@/hooks/useNotifications";
+import { useEffect } from "react";
+import { BsTwitter } from "react-icons/bs";
+
+interface NotificationFeedProps {}
 
 const NotificationFeed = () => {
-  return (
-    <div>NotificationFeed</div>
-  )
-}
+  const { data: currentUser, mutate: mutateUser } = useCurrentUser();
+  const {data: fetchedNotification = []} = useNotifications(currentUser?.id);
 
-export default NotificationFeed
+  useEffect(()=> {
+    mutateUser();
+  }, [mutateUser]);
+
+  if(fetchedNotification.length === 0){
+    return (
+        <div className="text-neutral-600 text-center p-6 text-xl"> No Notifications</div>
+    )
+  }
+
+  return 
+  
+  (<div className="flex- flex-col">
+    {fetchedNotification.map((notification: Record<string,any>)=> (
+        <div key={notification.id} className="flex flex-row items-center p-6 gap-4 border-b-[1px] border-neutral-800">
+            <BsTwitter color="white" size={32}/>
+            <p>
+                {notification.body}
+            </p>
+        </div>
+    ))}
+
+  </div>);
+};
+
+export default NotificationFeed;
